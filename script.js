099f05e2bcc3603d48bbdf7c4225119e997ee893
Dmitry Filippov <demodima97@mail.ru>
2020-12-21 22:55:39 +0300

    готово

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-let profileButton = document.querySelector('.profile__button');
-let popUp = document.querySelector('.pop-up-container');
-let popUpCloser = document.querySelector('.pop-up__close');
-let formElement = document.querySelector('.pop-up');
-let likeButton = document.querySelectorAll('.elements__like-button');
-
+const popUp = document.querySelector('.pop-up-container');
+const formElement = popUp.querySelector('.pop-up');
+const nameInput = formElement.querySelector('.pop-up__input_name');
+const jobInput = formElement.querySelector('.pop-up__input_job');
+const popUpCloser = formElement.querySelector('.pop-up__close');
+const profileButton = document.querySelector('.profile__button');
+const name = document.querySelector('.profile__name');
+const job = document.querySelector('.profile__text');
+const likeButton = document.querySelectorAll('.elements__like-button');
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.pop-up__input_name');
-    let jobInput = document.querySelector('.pop-up__input_job');
-    let name = document.querySelector('.profile__name');
-    let job = document.querySelector('.profile__text');
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
-    closer();
+    closePopUp();
 }
 
-function opener() {
+function openPopUp() {
+    nameInput.setAttribute('value', name.textContent);
+    jobInput.setAttribute('value', job.textContent);
     popUp.classList.add('pop-up-container_opened');
 }
 
-function closer() {
+function closePopUp() {
     popUp.classList.remove('pop-up-container_opened');
 }
 
 
 formElement.addEventListener('submit', handleFormSubmit);
-profileButton.addEventListener('click', opener);
-popUpCloser.addEventListener('click', closer);
+profileButton.addEventListener('click', openPopUp);
+popUpCloser.addEventListener('click', closePopUp);
 
 for (let i = 0; i < likeButton.length; i += 1) {
     likeButton[i].addEventListener('click', function () {

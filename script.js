19862cb4f86163a27151a7e5570b7a90d2af8f31
Dmitry Filippov <demodima97@mail.ru>
2020-12-23 00:05:29 +0300

    hotfixes

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 const popUp = document.querySelector('.pop-up-container');
 const formElement = popUp.querySelector('.pop-up');
-const nameInput = formElement.querySelector('.pop-up__input_name');
-const jobInput = formElement.querySelector('.pop-up__input_job');
+const nameInput = formElement.querySelector('.pop-up__input_type_name');
+const jobInput = formElement.querySelector('.pop-up__input_type_job');
 const popUpCloser = formElement.querySelector('.pop-up__close');
 const profileButton = document.querySelector('.profile__button');
 const name = document.querySelector('.profile__name');
 const job = document.querySelector('.profile__text');
 const likeButton = document.querySelectorAll('.elements__like-button');
 
-function handleFormSubmit(evt) {
-    evt.preventDefault();
-    name.textContent = nameInput.value;
-    job.textContent = jobInput.value;
-    closePopUp();
-}
 
 function openPopUp() {
     nameInput.setAttribute('value', name.textContent);
@@ -26,12 +20,14 @@ function closePopUp() {
 }
 
 
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    closePopUp();
+}
+
+
 formElement.addEventListener('submit', handleFormSubmit);
 profileButton.addEventListener('click', openPopUp);
 popUpCloser.addEventListener('click', closePopUp);
-
-for (let i = 0; i < likeButton.length; i += 1) {
-    likeButton[i].addEventListener('click', function () {
-        likeButton[i].classList.toggle('elements__like-button_active');
-    });
-}

fea054fed63761b0ce4394ac181469d781be79a5
Dmitry Filippov <demodima97@mail.ru>
2021-02-02 19:22:06 +0300

    добавлен validate.js

--- scripts/index.js
+++ scripts/index.js
@@ -102,43 +102,7 @@ function renderCard(card, wrap) {
   wrap.prepend(card);
 };
 
-function showInputError(element, errorMessage) {
-  const formError = element.closest('.pop-up__form').querySelector(`.${element.id}-error`);
-  element.classList.add('pop-up__input_type_error');
-  formError.textContent = errorMessage;
-  formError.classList.add('pop-up__input-error_active');
-};
 
-function hideInputError(element) {
-  const formError = element.closest('.pop-up__form').querySelector(`.${element.id}-error`);
-  element.classList.remove('pop-up__input_type_error');
-  formError.classList.remove('pop-up__input-error_active');
-}
-
-function isValid(input) {
-  if (input.validity.valid) {
-    hideInputError(input);
-  } else {
-    showInputError(input, input.validationMessage);
-  }
-}
-
-function hasInvalidInput(inputList) {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  })
-}
-
-function toggleButtonState(inputList, buttonElement) {
-  console.log(inputList);
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.remove('pop-up__submit_active');
-    console.log(true);
-  } else {
-    buttonElement.classList.add('pop-up__submit_active');
-    console.log(false);
-  }
-}
 
 formElement.addEventListener('submit', handleFormSubmit);
 profileButton.addEventListener('click', openPopUpProfile);

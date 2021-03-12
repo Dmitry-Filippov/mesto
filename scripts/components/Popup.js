56dd23ca5a08e9b8bf764e3f652ce5aa91fd0469
Dmitry Filippov <demodima97@mail.ru>
2021-03-12 13:38:13 +0300

    Popup class added

---
+++ scripts/components/Popup.js
@@ -0,0 +1,26 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popupSelector = popupSelector;
+  }
+
+  open() {
+    this._popupSelector.classList.add('pop-up-container_opened');
+  }
+
+  close() {
+    this._popupSelector.classList.remove('pop-up-container_opened');
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popupSelector.querySelector('.pop-up__close').addEventListener('click', () => {
+      this.close();
+    })
+  }
+}

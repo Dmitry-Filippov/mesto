49d5ca4b78acfa24304a87151f83358830828adb
Dmitry Filippov <demodima97@mail.ru>
2021-03-13 17:29:28 +0300

    PopupWithImage class added

---
+++ scripts/components/PopupWithImage.js
@@ -0,0 +1,15 @@
+import Popup from './Popup.js';
+import {openedImage, imageText} from '../utils/constants.js';
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+  }
+
+  open(link, name) {
+    openedImage.src = link;
+    openedImage.alt = name;
+    imageText.textContent = name;
+    super._popupSelector.classList.add('pop-up-container_opened');
+  }
+}
